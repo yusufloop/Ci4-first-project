cd445c1cc69f282742c7b3021746425b32a01c21
yusuf1 <yusufsmik@gmail.com>
2023-04-08 10:39:46 +0800

    tambah query builder

--- app/Models/CustomModel.php
+++ app/Models/CustomModel.php
@@ -9,6 +9,86 @@ class CustomModel{
     {
         $this->db =& $db;
     }
+
+    function all()
+    {
+        // "SELECT * FROM posts"
+
+        return $this ->db->table('posts')->get()->getResult();
+
+    }
+    function where()
+    {
+        // "SELECT * FROM posts"
+
+         $this->db->table('posts')
+                        ->where(['post_id >' => 90])
+                        ->where(['post_id <' => 95])
+                        ->where(['post_id !=' => 91])
+                        ->orderBy('post_id', 'ACS')
+                        ->get()
+                        ->getRow();
+
+    }
+    function join()
+    {
+        // "SELECT * FROM posts"
+
+        return $this->db->table('posts')
+                        ->where('post_id >', 50)
+                        ->where('post_id <', 60)
+                        ->join('users', 'posts.post_author = users.user_id', 'lrft')
+                        ->get()
+                        ->getResult();
+
+    }
+    function like()
+    {
+        // "SELECT * FROM posts"
+
+        return $this->db->table('posts')
+                        ->like('post_content', 'same', 'both') //%string% , %string , string%
+                        ->join('users', 'posts.post_author = users.user_id', 'lrft')
+                        ->get()
+                        ->getResult();
+
+    }
+    function grouping()
+    {
+        // "SELECT * FROM posts where (post_id = 25 AND post_date < 1990-01-01 00:00:00) OR post_author = 10"
+
+        return $this->db->table('posts')
+                        ->groupStart()
+                            ->where([
+                                'post_id >' => '25',
+                                'post_created_at >' => '1990-01-01 00:00:00'
+                                ])
+                        ->groupEnd()
+                        ->orWhere('post_author', 10)
+                        ->join('users', 'posts.post_author = users.user_id', 'lrft')
+                        ->get()
+                        ->getResult();
+
+    }
+    function whereIn()
+    {
+        $emails = ['glenda06@example.org','josefina.bechtelar@example.net','cyril.kub@example.net'];
+        return $this->db->table('posts')
+                        ->groupStart()
+                            ->where([
+                                'post_id >' => '25',
+                                'post_created_at >' => '1990-01-01 00:00:00'
+                                ])
+                        ->groupEnd()
+                        ->orWhereIn('email', $emails)
+                        ->join('users', 'posts.post_author = users.user_id', 'lrft')
+                        ->limit(5,4)
+                        ->get()
+                        ->getResult();
+
+    }
+
+
     function getPosts()
     {
         $builder = $this->db->table('posts');
